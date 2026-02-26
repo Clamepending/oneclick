f71f184a7f6fd216cc93ba9bef4018f1d4fba677
Mark Ogata <ogata@berkeley.edu>
2026-02-26 15:06:53 -0800

    Use new OneClick logo and align UI theme colors

--- src/components/layout/TopNav.tsx
+++ src/components/layout/TopNav.tsx
@@ -1,11 +1,20 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export function TopNav() {
   return (
     <header className="topbar">
       <div className="topbar-inner">
         <Link href="/" className="topbar-brand">
-          OneClick
+          <Image
+            src="/oneclicklogo.png"
+            alt="OneClick"
+            width={32}
+            height={32}
+            className="topbar-brand-logo"
+            priority
+          />
+          <span>OneClick</span>
         </Link>
         <Link href="/" className="button secondary">
           Home
